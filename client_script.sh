bf2a33d275d1c06bdf481be35086c81af6e54729
panospet <panospet@gmail.com>
2016-04-17 14:23:42 +0300

    Ask user for server IP

--- client_script.sh
+++ client_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 USERNAME=test
 PASSWORD=test
 
@@ -8,17 +10,26 @@ CLIENTCRT="client.crt"
 CLIENTKEY="client.key"
 CLIENTCONF="client.conf"
 
-openssl s_client -showcerts -connect 83.212.116.170:443/login </dev/null 2>/dev/null|openssl x509 -outform PEM > mycertfile.pem
+read -p "Please give the server's ip" $answer
 
-curl --cacert mycertfile.pem --cookie-jar cookies_temp 'https://83.212.116.170/login' --data 'username=test&password=test&submit=Login'
+if [[ -z "${answer// }" ]]; then
+    SERVER_IP="83.212.116.170"
+else
+    SERVER_IP=$answer
+fi
 
-curl --cacert mycertfile.pem --cookie cookies_temp 'https://83.212.116.170/cacert' > $CACERTFILE
 
-curl --cacert mycertfile.pem --cookie cookies_temp 'https://83.212.116.170/clientcert' > $CLIENTCRT
+openssl s_client -showcerts -connect $SERVER_IP:443/login </dev/null 2>/dev/null|openssl x509 -outform PEM > mycertfile.pem
 
-curl --cacert mycertfile.pem --cookie cookies_temp 'https://83.212.116.170/clientkey' > $CLIENTKEY
+curl --cacert mycertfile.pem --cookie-jar cookies_temp 'https://'$SERVER_IP'/login' --data 'username=test&password=test&submit=Login'
 
-curl --cacert mycertfile.pem --cookie cookies_temp 'https://83.212.116.170/logout'
+curl --cacert mycertfile.pem --cookie cookies_temp 'https://'$SERVER_IP'/cacert' > $CACERTFILE
+
+curl --cacert mycertfile.pem --cookie cookies_temp 'https://'$SERVER_IP'/clientcert' > $CLIENTCRT
+
+curl --cacert mycertfile.pem --cookie cookies_temp 'https://'$SERVER_IP'/clientkey' > $CLIENTKEY
+
+curl --cacert mycertfile.pem --cookie cookies_temp 'https://'$SERVER_IP'/logout'
 
 rm -rf cookies_temp mycertfile.pem
 
@@ -30,7 +41,7 @@ echo "client" >> $CLIENTCONF
 
 echo "dev tun" >> $CLIENTCONF
 
-echo "remote 83.212.116.170" >> $CLIENTCONF
+echo "remote $SERVER_IP" >> $CLIENTCONF
 
 echo "ca $CACERTFILE" >> $CLIENTCONF
 
@@ -52,22 +63,22 @@ echo "user $USER" >> $CLIENTCONF
 
 echo "group $USER" >> $CLIENTCONF
 
-clear
+#clear
 echo "Starting VPN connection in 5 seconds, press CTRL-C anytime to stop"
 sleep 1
-clear
+#clear
 echo "Starting VPN connection in 4 seconds, press CTRL-C anytime to stop"
 sleep 1
-clear
+#clear
 echo "Starting VPN connection in 3 seconds, press CTRL-C anytime to stop"
 sleep 1
-clear
+#clear
 echo "Starting VPN connection in 2 seconds, press CTRL-C anytime to stop"
 sleep 1
-clear
+#clear
 echo "Starting VPN connection in 1 seconds, press CTRL-C anytime to stop"
 sleep 1
-clear
+#clear
 
 echo "Starting OpenVPN..."
 sleep 2
@@ -76,6 +87,5 @@ sudo openvpn --config client.conf
 
 rm -rf $CLIENTCRT $CLIENTKEY $CLIENTCONF $CACERTFILE
 
-clear
 echo "Thank you for using our VPN service! :-)"
 
