5138d4e068ace00aebea575a561f9836b7c6341c
panospet <panospet@gmail.com>
2016-04-23 12:58:14 +0300

    added user class

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash, current_app, send_file, escape
 from flask.ext.login import LoginManager
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id',db.Integer , primary_key=True)
+    username = db.Column('username', db.String(20), unique=True , index=True)
+    password = db.Column('password' , db.String(10))
+    email = db.Column('email',db.String(50),unique=True , index=True)
+    registered_on = db.Column('registered_on' , db.DateTime)
+
+    def __init__(self , username ,password , email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.registered_on = datetime.utcnow()
+
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/')
 def hello_world():
