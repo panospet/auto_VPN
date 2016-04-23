67ea743157dc009ac1835a9a1c6a6439e4f3759a
panospet <panospet@gmail.com>
2016-04-23 20:46:56 +0300

    cosmetics

--- FlaskApp/add_user_to_db.py
+++ FlaskApp/add_user_to_db.py
@@ -1,8 +1,8 @@
 import auth
 
 if __name__ == '__main__':
-    username = raw_input("Please give username")
-    password = raw_input("Please give password")
+    username = raw_input("Please give username: ")
+    password = raw_input("Please give password: ")
     email = raw_input("Please give email")
 
     user = auth.User(username, password, email)
