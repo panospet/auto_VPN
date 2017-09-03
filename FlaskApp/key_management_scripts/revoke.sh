cfbe36565d120c00043d6ee24a7a0b77d0f4e697
panospet <panospet@gmail.com>
2017-09-03 13:16:34 +0300

    Revoke/unrevoke scripts make checks first

--- FlaskApp/key_management_scripts/revoke.sh
+++ FlaskApp/key_management_scripts/revoke.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-source /usr/share/easy-rsa/vars 
-/usr/share/easy-rsa/revoke-full $1
+keys_index_file=/usr/share/easy-rsa/keys/index.txt
+fileline="$(grep "/CN=$1/" $keys_index_file)"
+columns_number="$(echo $fileline | awk -F' ' '{print NF;}')"
 
-{
-    sleep 3
-    echo kill $1
-    sleep 3
-    echo exit
-} | telnet localhost 7505
+if [[ $columns_number -eq 5 ]] && [[ $fileline == V* ]]; then
 
+    source /usr/share/easy-rsa/vars 
+    /usr/share/easy-rsa/revoke-full $1
+
+    {
+        sleep 3
+        echo kill $1
+        sleep 3
+        echo exit
+    } | telnet localhost 7505
+
+    echo "Client certificate revoked successfully."
+    exit 0;
+
+elif [[ $columns_number -eq 6 ]] && [[ $fileline == R* ]]; then
+
+    echo "Client certificate is already revoked."
+    exit 0;
+
+else
+
+    echo "Error; key index file may be corrupted."
+    exit 1;
+fi
--- FlaskApp/key_management_scripts/unrevoke.sh
+++ FlaskApp/key_management_scripts/unrevoke.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
 keys_index_file=/usr/share/easy-rsa/keys/index.txt
-
 linenumber="$(grep -n "/CN=$1/" $keys_index_file | cut -f1 -d:)"
-echo $linenumber
-
 fileline="$(grep -n "/CN=$1/" $keys_index_file)"
-echo $fileline
+line="$(grep "/CN=$1/" $keys_index_file)"
 
-column2="$(echo $fileline | awk '{print $2}')"
-echo $column2
+columns_number="$(echo $line | awk -F' ' '{print NF;}')"
+echo $columns_number
 
-column4="$(echo $fileline | awk '{print $4}')"
-echo $column4
 
-column5="$(echo $fileline | awk '{print $5}')"
-echo $column5
 
-column6="$(echo $fileline | awk '{print $6}')"
-echo $column6
+if [[ $columns_number -eq 6 ]] && [[ $line == R* ]]; then
 
-echo -e "V\t$column2\t\t$column4\t$column5\t$column6" >> $keys_index_file
+    column2="$(echo $fileline | awk '{print $2}')"
+    column4="$(echo $fileline | awk '{print $4}')"
+    column5="$(echo $fileline | awk '{print $5}')"
+    column6="$(echo $fileline | awk '{print $6}')"
+    echo -e "V\t$column2\t\t$column4\t$column5\t$column6" >> $keys_index_file
+    sed -i "${linenumber}d" $keys_index_file
+    cd /usr/share/easy-rsa; source ./vars; ./generate_crl_file.sh
 
-sed -i "${linenumber}d" $keys_index_file
+    echo "Certificate unrevoked successfully."
+    exit 0;
 
-# source /usr/share/easy-rsa/vars
-# openssl ca -gencrl -out "/usr/share/easy-rsa/keys/crl.pem" -config "$KEY_CONFIG"
-cd /usr/share/easy-rsa; source ./vars ; ./generate_crl_file.sh
+elif [[ $columns_number -eq 5 ]] && [[ $fileline == V* ]]; then
+
+    echo "Certificate is already unrevoked and active"
+    exit 0;
+
+else
+
+    echo "Error; Key index file may be corrupted."
+    exit 1;
+
+fi
